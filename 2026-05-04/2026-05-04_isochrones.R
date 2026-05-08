09c2560fff62e79f1c3806173be9bfaa43da15be
Joseph Mohr <jmohr@thenightwatch.co>
2026-05-07 23:28:33 -0500

    code review 2026-05-07

--- 2026-05-04/2026-05-04_isochrones.R
+++ 2026-05-04/2026-05-04_isochrones.R
@@ -2,23 +2,24 @@
 source("R/toolkit.R")
 library(mapboxapi)
 library(osmdata)
+library(tigris)
+
 options(tigris_use_cache = TRUE)
 
 # import ----
-office <- mb_geocode("15502 Galveston Rd, Webster, TX 77598",
+office1 <- mb_geocode("15502 Galveston Rd, Webster, TX 77598",
+  output = "sf"
+)
+office1_iso <- mb_isochrone(office1, time = c(15, 30, 45))
+
+office2 <- mb_geocode("420 Garden Oaks Blvd, Houston, TX 77018",
   output = "sf"
 )
 
-office2  <- mb_geocode("420 Garden Oaks Blvd, Houston, TX 77018",
-                       output = "sf"
-)
+office2_iso <- mb_isochrone(office2, time = c(15, 30, 45))
 
-office_iso <- mb_isochrone(office, time = c(15, 30, 45))
-
-office_iso2 <- mb_isochrone(office2, time = c(15, 30, 45))
-
-
-cbsa_tx <- core_based_statistical_areas(cb = T)
+harris_co <- counties(state = 48, cb = T) %>%
+  filter(NAME == "Harris")
 
 htx_roads <- st_read("2026-05-04/data/Hwy/Hwy.shp")
 
@@ -28,8 +29,10 @@ htx_roads <- st_read("2026-05-04/data/Hwy/Hwy.shp")
 
 # visualize ----
 ggplot() +
-  geom_sf(data = htx_roads,
-          color = "black")+
+  geom_sf(
+    data = htx_roads,
+    color = "black"
+  ) +
   geom_sf(
     data = harris_co,
     color = "black",
@@ -37,13 +40,13 @@ ggplot() +
     size = 1
   ) +
   geom_sf(
-    data = office_iso,
+    data = office1_iso,
     aes(color = as.factor(time)),
     fill = NA,
     size = 1
   ) +
   geom_sf(
-    data = office,
+    data = office1,
     shape = 21,
     color = "black",
     fill = "salmon1"
@@ -61,8 +64,10 @@ ggplot() +
   map_theme()
 
 ggplot() +
-  geom_sf(data = htx_roads,
-          color = "black")+
+  geom_sf(
+    data = htx_roads,
+    color = "black"
+  ) +
   geom_sf(
     data = harris_co,
     color = "black",
@@ -70,7 +75,7 @@ ggplot() +
     size = 1
   ) +
   geom_sf(
-    data = office_iso2,
+    data = office2_iso,
     aes(color = as.factor(time)),
     fill = NA,
     size = 1
@@ -80,7 +85,8 @@ ggplot() +
     shape = 21,
     color = "black",
     fill = "salmon1"
-  ) + scale_color_viridis(
+  ) +
+  scale_color_viridis(
     option = "D",
     name = "Drive Time",
     discrete = T,
@@ -91,4 +97,3 @@ ggplot() +
     subtitle = "15, 30, and 45 Minutes"
   ) +
   map_theme()
-
